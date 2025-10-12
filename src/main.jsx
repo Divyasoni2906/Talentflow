f830f6ea6d6635d9bebe70d7416b8cf8014cab92
Divyasoni2906 <divyasoni2906@gmail.com>
2025-10-12 14:26:33 +0530

    Refresh error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,21 @@ import { store } from './app/store';
 import App from './App';
 import './index.css';
 
-async function enableMocking() {
-  const { worker } = await import('./features/mocks/browser');
-  return worker.start();
-}
+async function main() {
+  // We only enable mocking in the development environment.
+  // When you switch to a real backend, you can remove this entire function.
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./features/mocks/browser');
+    await worker.start();
+  }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-enableMocking().then(() => {
-  root.render(
+  ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>
   );
-});
+}
+main();
 
