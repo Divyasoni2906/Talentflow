06f08af36e1a3697aa23b53d9a947470c8a04fe7
Divyasoni2906 <divyasoni2906@gmail.com>
2025-10-12 14:58:19 +0530

    error fixed

--- public/mockServiceWorker.js
+++ public/mockServiceWorker.js
@@ -172,7 +172,7 @@ async function handleRequest(event, requestId, requestInterceptedAt) {
  * that registered the worker. It's with the latter the worker should
  * communicate with during the response resolving phase.
  * @param {FetchEvent} event
- * @returns {Promise<client | undefined>}
+ * @returns {Promise<Client | undefined>}
  */
 
 async function resolveMainClient(event) {
